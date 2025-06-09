590e9fa6231fa9477239f0ecd785e103865a60d1
jrtrj <jerit.reji.dev@gmail.com>
2025-06-09 18:34:45 +0530

    Deserialize the User.json

---
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,11 @@
+package org.example.entities;
+
+
+public class Ticket {
+    private String ticketID;
+    private String userId;
+    private String source;
+    private String destination;
+    private String dateOfTravel;
+    private Train train;
+}
---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,22 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class UserBookingService {
+    private User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USER_PATH = "../localDB/Users.json";
+
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        File users = new File(USER_PATH);
+        userList = objectMapper.readValue(users,new TypeReference<List<User>>(){});
+    }
+}
