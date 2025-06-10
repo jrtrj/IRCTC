1085ee56081ee0725658880dad6ff20285d92681
jrtrj <jerit.reji.dev@gmail.com>
2025-06-11 01:08:32 +0530

    added util service UserServiceUtil for password generation and verification

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -2,6 +2,59 @@ package org.example.entities;
 
 
 public class Ticket {
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, dateOfTravel);
+    }
+
     private String ticketID;
     private String userId;
     private String source;
