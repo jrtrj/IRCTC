59f783e3926a9e8d61e9fdc9a83fc97eb92ff2ce
jrtrj <jerit.reji.dev@gmail.com>
2025-06-12 13:55:48 +0530

    SignUp,login and fetch Bookings

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.example.entities.Ticket;
+import org.example.entities.User;
+import org.example.services.TrainService;
 import org.example.services.UserBookingService;
+import org.example.util.UserServiceUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class App {
     public String getGreeting() {
@@ -14,7 +21,7 @@ public class App {
         System.out.println(new App().getGreeting());
         Scanner scanner = new Scanner(System.in);
         int option = 0;
-        UserBookingService userBookingService;
+        UserBookingService userBookingService = null;
         try {
             userBookingService = new UserBookingService();
         } catch (IOException e) {
@@ -30,7 +37,53 @@ public class App {
             System.out.println("6. Cancel my Booking");
             System.out.println("7. Exit the App");
             option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the username to signup");
+                    String nameToSignUp = scanner.nextLine();
+                    System.out.println("Enter the password to signup");
+                    String passwordToSignUp = scanner.nextLine();
+                    User userToSignUp = new User(
+                            nameToSignUp,
+                            passwordToSignUp,
+                            UserServiceUtil.hashedPassword(passwordToSignUp),
+                            new ArrayList<Ticket>(),
+                            UUID.randomUUID().toString());
+                    try{
+                        new UserBookingService().signUp(userToSignUp);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
 
+                case 2:
+                    System.out.println("Enter the username to login");
+                    String nameToLogin = scanner.nextLine();
+                    System.out.println("Enter the password to login");
+                    String passwordToLogin = scanner.nextLine();
+                    User userToLogin = new User();
+                    userToLogin.setName(nameToLogin);
+                    userToLogin.setPassword(passwordToLogin);
+                    try {
+                        userBookingService = new UserBookingService(userToLogin);
+                        if(userBookingService.loginUser()) {
+                            System.out.println("Login Successful");
+                        }
+                        else
+                            System.out.println("Invalid Credentials");
+                    }
+                    catch (IOException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Fetching your Bookings");
+                    userBookingService.fetchBooking();
+                    break;
+                case 4:
+                    TrainService.getTrains();
+                    break;
+            }
         }
     }
 }
